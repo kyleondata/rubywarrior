fa064b334e00085dbbd1c5f23e0b32210c628534
kyleondata <kjennings.dev@gmail.com>
2012-10-01 22:02:24 -0500

    made it to level 7

--- kyle-beginner/player.rb
+++ kyle-beginner/player.rb
@@ -1,22 +1,51 @@
 class Player
-  @health
-
+@health
+$direction = :backward
   def play_turn(warrior)
-    # add your code here
-    puts "health is #{@health.to_s()} | #{warrior.health}"
-    takenDamage = @health.to_i() > warrior.health
-  	if warrior.feel.empty?
-      
-      if takenDamage
-        warrior.rest!
-      else
-	      warrior.walk!
-      end
-		else
-	    warrior.attack!
-	  end
+	# as a warrior I should only rest
+	# if I am not taking damage. Thus
+	# we can assume that if I the space
+	# in front of me is not empty I am taking
+	# damage. Also if my last turns health is greater
+	# than the current turns health I am taking damage. 
 
-    @health = warrior.health
-   
+	if atWall?(warrior)
+		$direction = :forward	
+	elsif !empty?(warrior)
+		takeAction(warrior)
+	else
+		if !takingDamage(warrior) && warrior.health < 20
+			warrior.rest!
+		else
+			if warrior.health > 15
+				warrior.walk!($direction)
+			elsif $direction == :forward
+				warrior.walk!(:backward)
+			end
+		end	
+	end
+    	@health = warrior.health
   end
+
+  def takingDamage(warrior)
+	@health > warrior.health if !@health.nil?
+  end
+
+  def takeAction(warrior)
+ 	if warrior.feel($direction).captive?
+		warrior.rescue!($direction)
+	else
+		warrior.attack!($direction)
+	end
+  end
+
+  def empty?(warrior)
+	warrior.feel($direction).empty?
+  end
+
+  def atWall?(warrior)
+	warrior.feel($direction).to_s() == "wall"
+  end
+ 
 end
+
